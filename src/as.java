6b1195330e25b432a65122e2de2901825a803992
priyamlulla <priyamlulla@gmail.com>
2025-05-23 00:29:39 +0530

    first commit learning post some code changes

--- src/as.java
+++ src/as.java
@@ -4,12 +4,13 @@ class VectorRemoveElement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Vector<String> vector = new Vector<>();
-        vector.add("Apple");
-        vector.add("Banana");
+
+        //vector.add("Banana");
         vector.add("Orange");
         vector.add("Mango");
         vector.add("Orange");
         vector.add("Cherry");
+        vector.add("Leo");
         System.out.println("Original Vector: " + vector);
         System.out.print("Enter the element to remove: ");
         String elementToRemove = scanner.nextLine();
